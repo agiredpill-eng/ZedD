551c599f013d98ec2fb75990cf7352f26a3d826a
agiredpill-eng <agiredpill@gmail.com>
2026-04-01 11:36:16 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import Navigation from './components/Navigation';
 import Hero from './sections/Hero';
 import About from './sections/About';
+import TheSigns from './sections/TheSigns';
+import TheMessage from './sections/TheMessage';
+import TheMessengers from './sections/TheMessengers';
+import YouWereMade from './sections/YouWereMade';
+import FlawlessDesign from './sections/FlawlessDesign';
+import TheInvitation from './sections/TheInvitation';
+import Connect from './sections/Connect';
 import Footer from './sections/Footer';
 
 function App() {
@@ -13,6 +20,13 @@ function App() {
       <main>
         <Hero />
         <About />
+        <TheSigns />
+        <TheMessage />
+        <TheMessengers />
+        <YouWereMade />
+        <FlawlessDesign />
+        <TheInvitation />
+        <Connect />
       </main>
       <Footer />
     </>
